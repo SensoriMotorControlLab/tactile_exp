f68aaeeb762c6fe7c1e148230896b1e6bc4ab17d
Marius 't Hart <thartbm@gmail.com>
2025-05-28 13:22:22 -0400

    better staircase values

--- doubleStaircases.py
+++ doubleStaircases.py
@@ -347,8 +347,8 @@ def setupStaircases(cfg):
     # varying strengths:
     steps = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99, 101, 103, 105, 107, 109, 111, 113, 115, 117, 119, 121, 123, 125, 127]
     
-    minTrials = 45
-    minReversals = 15
+    minTrials = 40
+    minReversals = 12
 
     staircases = []
 
@@ -361,41 +361,41 @@ def setupStaircases(cfg):
                 'stepvalue' : 'strength'}
 
         staircases.append( DescreasingStepStaircase(
-            steps        = range(0,128,4),
+            steps        = list(range(1,128,3)),
             idx          = 0,
             minTrials    = minTrials,
             minReversals = minReversals,
-            step         = 10,
+            step         = 8,
             decrease     = 1,
             info         = info
         ) )
 
         staircases.append( DescreasingStepStaircase(
-            steps        = range(2,128,4),
-            idx          = len(range(2,128,4))-1,
+            steps        = list(range(1,128,3)),
+            idx          = len(range(1,128,3))-1,
             minTrials    = minTrials,
             minReversals = minReversals,
-            step         = 10,
+            step         = 8,
             decrease     = 1,
             info         = info
         ) )
 
         staircases.append( DescreasingStepStaircase(
-            steps        = range(1,128,4),
+            steps        = list(range(1,128,3)),
             idx          = 0,
             minTrials    = minTrials,
             minReversals = minReversals,
-            step         = 10,
+            step         = 8,
             decrease     = 1,
             info         = info
         ) )
 
         staircases.append( DescreasingStepStaircase(
-            steps        = range(3,128,4),
-            idx          = len(range(3,128,4))-1,
+            steps        = list(range(1,128,3)),
+            idx          = len(range(1,128,3))-1,
             minTrials    = minTrials,
             minReversals = minReversals,
-            step         = 10,
+            step         = 8,
             decrease     = 1,
             info         = info
         ) )
